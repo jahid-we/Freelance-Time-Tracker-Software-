2f85e344a2ac4821c97f8d37f39309b9fc0d0032
jahid-we <jahidhasan370919@gmail.com>
2025-06-02 22:21:51 +0600

    Inertia Vue Setup

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Foundation\Application;
+use App\Http\Middleware\HandleInertiaRequests;
 use Illuminate\Foundation\Configuration\Exceptions;
 use Illuminate\Foundation\Configuration\Middleware;
 
@@ -15,6 +16,9 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->validateCsrfTokens(except: [
             '*',
         ]);
+        $middleware->web(append: [
+            HandleInertiaRequests::class,
+    ]);
     })
     ->withExceptions(function (Exceptions $exceptions) {
         //
