e38cf7403a89787a752e16f1c846adac12259ec5
jahid-we <jahidhasan370919@gmail.com>
2025-07-01 22:23:40 +0600

    CSRF Protection Enable

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -13,9 +13,9 @@ return Application::configure(basePath: dirname(__DIR__))
         health: '/up',
     )
     ->withMiddleware(function (Middleware $middleware) {
-        $middleware->validateCsrfTokens(except: [
-            '*',
-        ]);
+        // $middleware->validateCsrfTokens(except: [
+        //     '*',
+        // ]);
         $middleware->web(append: [
             HandleInertiaRequests::class,
         ]);
